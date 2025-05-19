69fc17421972867fc6a2fca82e562792cc2ab42c
Adriana Hristova <117771916+Adr1ana79@users.noreply.github.com>
2025-05-19 15:35:19 +0300

    Add files via upload
    Включване на допълнителни линкове и h1, h2… с markdown при създаване на блогове.

---
+++ MarkdownUtil.java
@@ -0,0 +1,12 @@
+import org.commonmark.node.*;
+import org.commonmark.parser.Parser;
+import org.commonmark.renderer.html.HtmlRenderer;
+
+public class MarkdownUtil {
+    public static String markdownToHtml(String markdownText) {
+        Parser parser = Parser.builder().build();
+        Node document = parser.parse(markdownText);
+        HtmlRenderer renderer = HtmlRenderer.builder().build();
+        return renderer.render(document);
+    }
+}
