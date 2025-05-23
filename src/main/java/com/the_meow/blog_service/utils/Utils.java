0d42b90d5fc57dc569f5c0320b339c8c4a762d44
moris071651 <moris071651@proton.me>
2025-05-23 07:59:12 +0300

    MERGE: compression utils file to generic utils file

--- src/main/java/com/the_meow/blog_service/utils/Utils.java
+++ src/main/java/com/the_meow/blog_service/utils/Utils.java
@@ -1,5 +1,13 @@
 package com.the_meow.blog_service.utils;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
+
 public class Utils {
     public static Integer getUserId(String token) {
         try {
@@ -8,5 +16,54 @@ public class Utils {
         catch (NumberFormatException e) {
             return null;
         }
+    }
+
+    public String compressText(String text) throws IOException {
+        if (text.length() <= 100) {
+            return text;
+        }
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        GZIPOutputStream gzipOutputStream = new GZIPOutputStream(byteArrayOutputStream);
+
+        gzipOutputStream.write(text.getBytes());
+        gzipOutputStream.close();
+
+        return Base64.getEncoder().encodeToString(byteArrayOutputStream.toByteArray());
+    }
+
+    public String decompressText(String text) {
+        if (text == null || text.isEmpty()) {
+            return text;
+        }
+
+        byte[] decoded;
+        try {
+            decoded = Base64.getDecoder().decode(text);
+        } catch (IllegalArgumentException e) {
+            return null;
+        }
+    
+        ByteArrayInputStream byteStream = new ByteArrayInputStream(decoded);
+        GZIPInputStream gzip;
+        try {
+            gzip = new GZIPInputStream(byteStream);
+        } catch (IOException e) {
+            return null;
+        }
+    
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        byte[] buffer = new byte[10_000];
+        int len;
+    
+        try {
+            while ((len = gzip.read(buffer)) != -1) {
+                out.write(buffer, 0, len);
+            }
+        } catch (IOException e) {
+            return null;
+        }
+    
+        return out.toString(StandardCharsets.UTF_8);
     }
 }
