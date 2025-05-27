1b02f11929563841f246b6cda0a30b7c3ab17481
Phantomvv1 <krisi_andreev@abv.bg>
2025-05-27 13:16:51 +0300

    Making the compression util better

--- src/main/java/com/the_meow/blog_service/utils/CompressionUtil.java
+++ src/main/java/com/the_meow/blog_service/utils/CompressionUtil.java
@@ -10,25 +10,22 @@ import java.util.Base64;
 import java.util.zip.GZIPInputStream;
 import java.util.zip.GZIPOutputStream;
 
-@Data
-@AllArgsConstructor
-public class CompressionUtil {
-    private String blog;
+public abstract class CompressionUtil {
 
-    public String compress() throws IOException {
-        if (this.blog.length() <= 70) return this.blog;
+    public static String compress(String blog) throws IOException {
+        if (blog.length() <= 70) return blog;
 
         ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
         GZIPOutputStream gzipOutputStream = new GZIPOutputStream(byteArrayOutputStream);
 
-        gzipOutputStream.write(this.blog.getBytes());
+        gzipOutputStream.write(blog.getBytes());
         gzipOutputStream.close();
 
         return Base64.getEncoder().encodeToString(byteArrayOutputStream.toByteArray());
     }
 
-    public String decompress() throws IOException {
-        ByteArrayInputStream byteStream = new ByteArrayInputStream(Base64.getDecoder().decode(this.blog));
+    public static String decompress(String blog) throws IOException {
+        ByteArrayInputStream byteStream = new ByteArrayInputStream(Base64.getDecoder().decode(blog));
         GZIPInputStream gzip = new GZIPInputStream(byteStream);
         ByteArrayOutputStream out = new ByteArrayOutputStream();
 
